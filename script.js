85cf5e7aa8196aa595a2c353a5f37d7d93ac953a
ioursmid <127405614+ioursmid@users.noreply.github.com>
2024-09-07 17:31:04 -0300

    upload dos arquivos do meu site

---
+++ script.js
@@ -0,0 +1,204 @@
+const empresas = [
+    {
+        categoria: 'lojas-de-celulares',
+        logo: 'https://i.postimg.cc/xCGs2h7r/logoalfa.png',
+        descricao: 'Alfa Prime',
+        whatsapp: 'https://wa.me/5517999740086',
+        instagram: 'https://instagram.com/alfa.prime_',
+        localizacao: 'https://maps.app.goo.gl/jE7zd7NKHJ5E4W7F6'
+    },
+    {
+        categoria: 'restaurantes',
+        logo: 'https://i.postimg.cc/pTLXHbG4/logoescarpas.png',
+        descricao: 'Escarpas Sushi Bar',
+        whatsapp: 'https://wa.me/5511999999999',
+        instagram: 'https://instagram.com/loja2',
+        localizacao: 'https://goo.gl/maps/exemplo2'
+    },
+    {
+        categoria: 'autoescolas',
+        logo: 'https://via.placeholder.com/100',
+        descricao: 'Loja 2 - Vendas de eletrônicos',
+        whatsapp: 'https://wa.me/5511999999999',
+        instagram: 'https://instagram.com/loja2',
+        localizacao: 'https://goo.gl/maps/exemplo2'
+    },
+    {
+        categoria: 'drogarias',
+        logo: 'https://via.placeholder.com/100',
+        descricao: 'Loja 2 - Vendas de eletrônicos',
+        whatsapp: 'https://wa.me/5511999999999',
+        instagram: 'https://instagram.com/loja2',
+        localizacao: 'https://goo.gl/maps/exemplo2'
+    },
+    {
+        categoria: 'drogarias',
+        logo: 'https://via.placeholder.com/100',
+        descricao: 'Loja 2 - Vendas de eletrônicos',
+        whatsapp: 'https://wa.me/5511999999999',
+        instagram: 'https://instagram.com/loja2',
+        localizacao: 'https://goo.gl/maps/exemplo2'
+    },
+    {
+        categoria: 'drogarias',
+        logo: 'https://via.placeholder.com/100',
+        descricao: 'Loja 2 - Vendas de eletrônicos',
+        whatsapp: 'https://wa.me/5511999999999',
+        instagram: 'https://instagram.com/loja2',
+        localizacao: 'https://goo.gl/maps/exemplo2'
+    },
+    {
+        categoria: 'drogarias',
+        logo: 'https://via.placeholder.com/100',
+        descricao: 'Loja 2 - Vendas de eletrônicos',
+        whatsapp: 'https://wa.me/5511999999999',
+        instagram: 'https://instagram.com/loja2',
+        localizacao: 'https://goo.gl/maps/exemplo2'
+    },
+    {
+        categoria: 'drogarias',
+        logo: 'https://via.placeholder.com/100',
+        descricao: 'Loja 2 - Vendas de eletrônicos',
+        whatsapp: 'https://wa.me/5511999999999',
+        instagram: 'https://instagram.com/loja2',
+        localizacao: 'https://goo.gl/maps/exemplo2'
+    },
+    {
+        categoria: 'drogarias',
+        logo: 'https://via.placeholder.com/100',
+        descricao: 'Loja 2 - Vendas de eletrônicos',
+        whatsapp: 'https://wa.me/5511999999999',
+        instagram: 'https://instagram.com/loja2',
+        localizacao: 'https://goo.gl/maps/exemplo2'
+    },
+    {
+        categoria: 'drogarias',
+        logo: 'https://via.placeholder.com/100',
+        descricao: 'Loja 2 - Vendas de eletrônicos',
+        whatsapp: 'https://wa.me/5511999999999',
+        instagram: 'https://instagram.com/loja2',
+        localizacao: 'https://goo.gl/maps/exemplo2'
+    },
+    {
+        categoria: 'drogarias',
+        logo: 'https://via.placeholder.com/100',
+        descricao: 'Loja 2 - Vendas de eletrônicos',
+        whatsapp: 'https://wa.me/5511999999999',
+        instagram: 'https://instagram.com/loja2',
+        localizacao: 'https://goo.gl/maps/exemplo2'
+    },
+    {
+        categoria: 'drogarias',
+        logo: 'https://via.placeholder.com/100',
+        descricao: 'Loja 2 - Vendas de eletrônicos',
+        whatsapp: 'https://wa.me/5511999999999',
+        instagram: 'https://instagram.com/loja2',
+        localizacao: 'https://goo.gl/maps/exemplo2'
+    }
+    
+    // Acrescente até 50 empresas aqui
+];
+
+const listaEmpresas = document.getElementById('lista-empresas');
+
+// Função para filtrar empresas por categoria
+function exibirEmpresas(categoria) {
+    listaEmpresas.innerHTML = ''; // Limpa a lista atual
+    empresas.filter(empresa => empresa.categoria === categoria).forEach(empresa => {
+        const empresaDiv = document.createElement('div');
+        empresaDiv.classList.add('empresa');
+
+        empresaDiv.innerHTML = `
+            <img src="${empresa.logo}" alt="Logo da empresa" />
+            <p>${empresa.descricao}</p>
+            <div class="botoes">
+                <a href="${empresa.whatsapp}" target="_blank"><button><i class="fab fa-whatsapp"></i></button></a>
+                <a href="${empresa.instagram}" target="_blank"><button><i class="fab fa-instagram"></i></button></a>
+                <a href="${empresa.localizacao}" target="_blank"><button><i class="fas fa-map-marker-alt"></i></button></a>
+             </div>
+        `;
+
+        listaEmpresas.appendChild(empresaDiv);
+    });
+}
+
+// Exibir todas as empresas inicialmente
+empresas.forEach(empresa => {
+    const empresaDiv = document.createElement('div');
+    empresaDiv.classList.add('empresa');
+
+    empresaDiv.innerHTML = `
+        <img src="${empresa.logo}" alt="Logo da empresa" />
+        <p>${empresa.descricao}</p>
+        <div class="botoes">
+                <a href="${empresa.whatsapp}" target="_blank"><button><i class="fab fa-whatsapp"></i></button></a>
+                <a href="${empresa.instagram}" target="_blank"><button><i class="fab fa-instagram"></i></button></a>
+                <a href="${empresa.localizacao}" target="_blank"><button><i class="fas fa-map-marker-alt"></i></button></a>
+        </div>
+    `;
+
+    listaEmpresas.appendChild(empresaDiv);
+});
+
+// Script para função de slide
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Muda de imagem a cada 3 segundos
+}
+
+function plusSlides(n) {
+    slideIndex += n - 1;
+    showSlides();
+}
+
+function buscarEmpresa() {
+    const termoBusca = document.getElementById('search-input').value.toLowerCase();
+    listaEmpresas.innerHTML = ''; // Limpa a lista atual
+    const categorias = document.querySelector('.categorias'); // Seleciona o elemento de categorias
+
+    const resultados = empresas.filter(empresa =>
+        empresa.descricao.toLowerCase().includes(termoBusca)
+    );
+
+    if (resultados.length > 0) {
+        categorias.style.display = 'none'; // Esconde as categorias
+        resultados.forEach(empresa => {
+            const empresaDiv = document.createElement('div');
+            empresaDiv.classList.add('empresa');
+
+            empresaDiv.innerHTML = `
+                <img src="${empresa.logo}" alt="Logo da empresa" />
+                <p>${empresa.descricao}</p>
+                 <div class="botoes">
+                <a href="${empresa.whatsapp}" target="_blank"><button><i class="fab fa-whatsapp"></i></button></a>
+                <a href="${empresa.instagram}" target="_blank"><button><i class="fab fa-instagram"></i></button></a>
+                <a href="${empresa.localizacao}" target="_blank"><button><i class="fas fa-map-marker-alt"></i></button></a>
+        </div>
+            `;
+
+            listaEmpresas.appendChild(empresaDiv);
+        });
+    } else {
+        categorias.style.display ='none'; // Mostra as categorias novamente se não houver resultados
+        listaEmpresas.innerHTML = '<p>Nenhuma empresa encontrada.</p>';
+    }
+}
+
+function mostrarSomenteCategorias() {
+    const categorias = document.querySelector('.categorias');
+    categorias.style.display = ''; // Mostra as categorias novamente
+    listaEmpresas.innerHTML = ''; // Limpa os resultados da busca
+}
+
+
+
